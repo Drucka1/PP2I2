d4800fabe4053a9a77c10804982f493aabad33a9
LEGIERSKI Marine <marine.legierski@telecomnancy.eu>
2024-05-13 21:37:23 +0200

    ajout de rendertext.h

--- include/game/rendertext.h
+++ include/game/rendertext.h
@@ -1,7 +1,10 @@
 #ifndef RENDERTEXT_H
 #define RENDERTEXT_H
 
+#include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+
+void text_display(SDL_Renderer* renderer, TTF_Font* font, const char* text, int x, int y);
 
 
 #endif // !RENDERTEXT_H
